5d7a29933a35fa633fc9c44e17d79cac68ed5b51
Jan Suchotzki <jan@suchotzki.de>
2015-03-17 15:53:45 +0100

    Erster Draft

---
+++ Code/HalloDocker.cs
@@ -0,0 +1,19 @@
+/// <summary>
+/// Einfacher C#-Quellcode um zuzeigen wie das offizielle 
+/// <see href="https://registry.hub.docker.com/u/library/mono/">Mono Docker Image</see>
+/// verwendet werden kann um C# Anwendungen zu erstellen und auszuführen.
+/// </summary>
+/// <remarks>
+/// Eine ausführliche Beschreibung zu diesem Beispiel kann in 
+/// <see href="">diesem Artikel</see> gefunden werden.
+/// </remarks>
+
+using System; // -> brauchen wir für die Verwendung von Console
+
+class HalloDocker
+{
+	static void Main(string[] args)
+	{
+        Console.WriteLine("inginform sagt Hallo aus einem Docker Container!");
+	}
+}
